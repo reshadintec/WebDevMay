c9eb1010421deef68f35510fb2003aa17f387aad
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-06-07 10:15:28 +0200

    javascript

---
+++ javascript/Tutorial 1/app.js
@@ -0,0 +1,29 @@
+let website = "www.google.com";
+//let website= "www.facebook.com";//
+website = "www.facebook.com";
+console.log(website);
+console.log(user);
+var user = "root"
+var username= "ahmad"
+console.log(username);
+var a=10;
+var b=20;
+b=a+2;
+var c=a+b;
+console.log(c);
+var city = "Bru" + "ssel";
+console.log(city);
+var country= 'Brussel\'s Country';
+console.log(country);
+document.getElementById("message").innerText="Don't Disturb"
+document.getElementById("container").innerHTML="<h1> Hello my dear </h1>"
+var weight=70;
+var height=174;
+var bmi=weight/height*height;
+console.log(bmi);
+//Net Salary//
+var bruto=3000;
+var taxper=0.21;
+var tax=bruto*taxper;
+var netto=bruto-tax;
+console.log(netto);
