399a3dc7b799aaca387b50bbdc268f6a23f64848
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-08-01 15:56:02 +0200

    merge

---
+++ javascript/DOM/Lesson 1/Lesson 2/app.js
@@ -0,0 +1,7 @@
+var result;
+result = document.getElementById("navbarSupportedContent");
+result = document.querySelector('.col');
+// result = document.querySelectorAll('.col');
+result = document.querySelectorAll('.row');
+
+console.log(result);
