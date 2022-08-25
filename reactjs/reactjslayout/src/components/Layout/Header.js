917326431c8e439233792d84abbe22082db2b3ec
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-08-25 11:03:21 +0200

    update

--- reactjs/reactjslayout/src/components/Layout/Header.js
+++ reactjs/reactjslayout/src/components/Layout/Header.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-     <nav>
-        <ul>
-            <li><Link to={'/'}> Homepage</Link></li>
-            <li><Link to={'/contact'}>Contact</Link></li>
-        </ul>
-    </nav>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Web Panda</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link to={'/'} className='nav-link text-decoration-none'>Home</Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
