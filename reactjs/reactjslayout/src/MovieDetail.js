238caebd81ee88c9355b5cfea3f7e85e89041bfb
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-08-29 13:19:51 +0200

    update

--- reactjs/reactjslayout/src/MovieDetail.js
+++ reactjs/reactjslayout/src/MovieDetail.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+// import bootstrap components
 import { Badge, Col, Container, ListGroup, Row } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 
@@ -13,29 +14,44 @@ function MovieDetail() {
             .then(data => {
                 setMovieDetail(data)
             })
-    }, [])
+    }, [movie_id])
     return (
-        <Container className='bg-dark p-0' fluid={true}>
-            <div className="p-5 bg-primary text-white movie-img " style={{'background':`url(https://image.tmdb.org/t/p/w500${movieDetail.poster_path})`}}>
+        <Container className="bg-dark p-0" fluid={true}>
+            <div className="p-5 bg-primary text-white movie-img rounded-0" style={{ 'background': `url(https://image.tmdb.org/t/p/w500${movieDetail.poster_path})` }}>
                 <h1 className='text-center movie-title'>{movieDetail.original_title}</h1>
             </div>
-            <Row className='p-0 m-0'>
+            <Row className="p-0 m-0">
                 <Col md={3} className="p-3">
-                <ListGroup>
-                    <ListGroup.Item className='bg-none text-light'>
-                    <Badge bg='warning' text='dark'> IMDB Score: <p className='bg-white rounded d-inline'>{Math.floor(movieDetail.vote_average)}</p></Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item className='bg-none text-light'>
-                    <Badge bg='warning' text='dark'> Movie Language: <p className='bg-white rounded d-inline'>{movieDetail.original_language!==undefined ?
-                    movieDetail.original_language.toUpperCase()
-                    :
-                    ''
-                    }</p></Badge>
-                    </ListGroup.Item>
-                    <ListGroup.Item className='bg-none text-light'>
-                    <Badge bg='warning' text='dark'> Category: <p className='bg-white rounded d-inline'>{}</p></Badge>
-                    </ListGroup.Item>
-                </ListGroup>
+                    <ListGroup>
+                        <ListGroup.Item className="bg-none text-light border-0">
+                            <Badge bg="warning" text="dark">ImDB Score: <p className='bg-white rounded d-inline'>
+                                {Math.floor(movieDetail.vote_average)}
+                            </p></Badge>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="bg-none text-light border-0">
+                            <Badge bg="warning" text="dark">Movie Language: <p className='bg-white rounded d-inline'>{
+                                movieDetail.original_language !== undefined ?
+                                    movieDetail.original_language.toUpperCase()
+                                    :
+                                    ''
+                            }</p></Badge>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="bg-none text-light border-0">
+                            <Badge bg="warning" text="dark">Category: <p className='bg-white rounded d-inline'>
+                                {
+                                    movieDetail.genres !== undefined ?
+                                        movieDetail.genres.map((cat, index) => (
+                                            <><p className='d-inline' key={cat.id}>{cat.name}</p>, </>
+                                        )) :
+                                        ''
+                                }
+                            </p></Badge>
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Col>
+                <Col md={8}>
+
                 </Col>
             </Row>
         </Container>
