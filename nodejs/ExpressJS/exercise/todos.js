2758556e5c38a331bc730d6e364cb051a9edea04
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-09-27 14:07:29 +0200

    update

--- nodejs/ExpressJS/exercise/todos.js
+++ nodejs/ExpressJS/exercise/todos.js
@@ -4,56 +4,21 @@ require('dotenv').config()
 const app = express()
 const TodoModel = require("./model")
 mongoose.connect(process.env.MONGO_URI, (err) => console.log("connected"))
-
+const {home,todos,saveTodo,notfound} = require('./controller')
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send("Welcome to todos restful api v1.0")
-})
+app.get('/', home)
 
 // get all todos
-app.get('/todos', (req, res) => {
-    console.log(req.query.username)
-    if (req.query.title !== undefined && req.query.username !== undefined) {
-        TodoModel.find({ title: { $regex: req.query.title }, username: req.query.username })
-            .then(results => {
-                res.json({
-                    message: 'ok',
-                    todos: results
-                })
-            })
-    } else if (req.query.title !== undefined) {
-        TodoModel.find({ title: { $regex: req.query.title } })
-            .then(results => {
-                res.json({
-                    message: 'ok',
-                    todos: results
-                })
-            })
-
-    } else {
-        TodoModel.find({})
-            .then(todos => res.json({ message: 'ok', todos: todos }))
-    }
-})
+app.get('/todos', todos)
 
 // save a todo
-app.post('/todo', (req, res) => {
-    let todo = new TodoModel(req.body)
-    todo.save()
-        .then(result => {
-            res.json({
-                message: 'ok',
-                todo: result
-            })
-        })
-})
-
-
+app.post('/todo', saveTodo)
 
 // not found endpoint
-app.get('*', (req, res) => {
-    res.json({ message: 'not found' })
-})
+app.get('*', notfound)
 
 app.listen(process.env.PORT)
+
