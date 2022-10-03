66789e758202aa4d7ceedd22fb2e7c55190ba9f6
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-10-03 08:53:28 +0200

    sync

--- nodejs/ExpressJS/multer/sandbox.js
+++ nodejs/ExpressJS/multer/sandbox.js
@@ -17,10 +17,32 @@ class User {
     }
 }
 
+class Admin extends User{
+    constructor(username,email,title){
+        super(username,email);
+        this.title=title;
+    }
+    deleteUser(user){
+        users = users.filter((u)=>{
+            return u !== user;
+        })
+    }
+}
+
 const user1 = new User('ahmad','ahmad@gmail.com');
 user1.login().incScore();
 const userTwo = new User('Mahmoud','Mahmoud@gmail.com');
 userTwo.incScore().login().incScore();
 const userThree = new User('Ali','ali@gmail.com');
 console.log(user1,userTwo,userThree);
-    
+const admin = new Admin('Reshad','admin@admin.com')
+let users = [user1,userTwo,userThree,admin];
+console.log(users);
+admin.deleteUser(user1);
+console.log(users);
+const userFour = new User('Arsenal','arsenal@ar.com');
+console.log(users);
+
+const newAdmin = new Admin('New Admin','admin@ad.com','super admin');
+newAdmin.login().incScore();
+console.log(newAdmin);
