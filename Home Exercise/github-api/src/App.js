6d4d93edd5b0e36bec80cb0dd200adbc30ca170c
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-10-21 18:26:55 +0200

    sync

--- Home Exercise/github-api/src/App.js
+++ Home Exercise/github-api/src/App.js
@@ -2,13 +2,10 @@ import './App.css'
 import React, { useState } from "react";
 
 function App() {
-  const [count , setCount] = useState(0);
+  
   return (
     <div className="app">
-      <button onClick={()=>{setCount(count+1)}}>Increase</button>
-      <button onClick={()=>{setCount(count-1)}}>Decrease</button>
-      <button onClick={()=>{setCount(0)}}>Reset</button>
-      {count}
+      
     </div>
   );
 }
