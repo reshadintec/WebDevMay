6d73555920ce671924f40f7f3aa761a23382dcf3
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-10-22 12:05:00 +0200

    sync

--- Home Exercise/github-api/src/App.js
+++ Home Exercise/github-api/src/App.js
@@ -2,10 +2,30 @@ import './App.css'
 import React, { useState } from "react";
 
 function App() {
-  
+  const [tasks,setTasks] = useState([]);
+  const[todo,setTodo] = useState("");
+  const handleChange = (e)=>{
+    setTodo(e.target.value);
+  }
+
+  const addTask = ()=>{
+    setTasks([...tasks,todo]);
+  }
   return (
     <div className="app">
-      
+      <div className="inputUi">
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Todo</button>
+      </div>
+
+      <div className="tasks">
+        {tasks.map((task ,key)=>{
+          return <div>
+            <h2 key={key}>{task}</h2>
+            <button> X </button>
+            </div>
+        })}
+      </div>
     </div>
   );
 }
