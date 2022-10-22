c788187718382f9668b220104e52d0344bd4e6d2
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-10-22 23:01:59 +0200

    sync

--- Home Exercise/github-api/src/App.js
+++ Home Exercise/github-api/src/App.js
@@ -2,31 +2,40 @@ import './App.css'
 import React, { useState } from "react";
 
 function App() {
+  
+// const deleteTask = (taskName)=>{
+    
+  // };
   const [todoList,setTodoList] = useState([]);
   const [newTask,setNewTask] = useState("");
-  const handleChange = (e)=>{
-    setNewTask (e.target.value);
+  const handleChange = (event)=>{
+    setNewTask(event.target.value)
   };
-  const addTask = ()=>{
-    setTodoList([...todoList,newTask]);
-  };
-
-  const deleteTask = (taskName)=>{
-    setTodoList(todoList.filter((task)=> task !== taskName));
-  };
-  
+  const addTask = (()=>{
+    const task = {
+      id: todoList.length===0 ? 1 : todoList[todoList.length-1].id +1 ,
+      taskName: newTask
+    }
+    setTodoList([...todoList,task]);
+  });
+  const deleteTask = (id)=>{
+    const newTodolist = todoList.filter((task)=>{
+      return task.id !== id;
+    })
+    setTodoList(newTodolist);
+  }
   return (
     <div className="app">
       <div className='addTask'>
         <input onChange={handleChange}/>
-        <button onClick={addTask}>Add New Task</button>        
+        <button onClick={addTask}>Add Task</button>  
       </div>
       <div className='list'>
       {todoList.map((task)=>{
         return <div>
-          <h2>{task}</h2>
-          <button onClick={()=>deleteTask(task)}>X</button>
-        </div>
+          <h2>{task.id}:{task.taskName}</h2>
+          <button onClick={()=>deleteTask(task.id)}>X</button>
+          </div>
       })}
       </div>
     </div>
