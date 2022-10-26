7a78cd748b85c7251c1f29f664b8b39705fca8a2
reshadintec <Ahmadreshad.Hassani@student.intecbrussel.be>
2022-10-26 13:02:46 +0200

    final edits

--- Home Exercise/my-portfolio/src/App.js
+++ Home Exercise/my-portfolio/src/App.js
@@ -5,6 +5,7 @@ import Experience from "./pages/Experience"
 import Projects from "./pages/Projects";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ProjectDisplay from './pages/ProjectDisplay';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/experience' activeClassName="selected" element={<Experience/>} />
+          <Route path='/project/:id' element={<ProjectDisplay/>} />
           <Route path='/projects' element={<Projects />} />
         </Routes>
         <Footer />
--- Home Exercise/my-portfolio/src/helpers/ProjectList.js
+++ Home Exercise/my-portfolio/src/helpers/ProjectList.js
@@ -4,14 +4,17 @@ import Proj3 from "../assets/proj3.webp"
 export const ProjectList = [
     {
         name: "THOF Website",
-        image: Proj1
+        image: Proj1,
+        skills: "WordPress, MYSQL, Elementor, Woocommerce, WodrPress Multisite, AWS Lightsail"
     },
     {
         name: "Movie Clone App",
-        image: Proj2
+        image: Proj2,
+        skills: "ReactJS, ReactBootstrap, HTML, CSS"
     },
     {
         name: "HB Perfumes",
-        image:Proj3
+        image:Proj3,
+        skills: "WordPress, MYSQL, Elementor, HTML, CSS"
     }
 ]
